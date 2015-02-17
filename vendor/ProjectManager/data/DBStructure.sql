961b172e91661dfc03df70fa27edc86960d78cb9
tal <tal@hunam.net>
2015-02-17 19:28:49 +0200

    Add rest of the tables required for first version
    Add all missing tables required by the project's first version's goals
    as described in commit a9b7b4caee236721803bcc5b6a2f3e3f28e84bb8. The
    following tables have been added to the database structure SQL:
    role, user_role, project, user_project_role, status, task
    
    SQL for the user table was added in a previous commit, but has had a
    minor edit.

--- vendor/ProjectManager/data/DBStructure.sql
+++ vendor/ProjectManager/data/DBStructure.sql
@@ -5,10 +5,95 @@ CREATE TABLE user (
 	name VARCHAR(255) NOT NULL,
 	public_key VARCHAR(255) NOT NULL DEFAULT '',
 	private_key VARCHAR(255) NOT NULL DEFAULT '',
-	deleted DATETIME,
+	deleted TINYINT UNSIGNED NOT NULL DEFAULT 0,
 
 	PRIMARY KEY (id),
 	INDEX (email),
 	INDEX (public_key),
+	INDEX (deleted)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+
+
+CREATE TABLE role (
+	id INT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+
+	PRIMARY KEY (id)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+INSERT INTO role (id, name) VALUES (1, 'admin');
+INSERT INTO role (id, name) VALUES (2, 'member');
+
+
+CREATE TABLE user_role (
+	id INT UNSIGNED AUTO_INCREMENT,
+	user_id INT UNSIGNED NOT NULL,
+	role_id INT UNSIGNED NOT NULL,
+	deleted TINYINT UNSIGNED NOT NULL DEFAULT 0,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	INDEX (deleted)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+
+
+CREATE TABLE project (
+	id INT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+	description TEXT,
+	deleted TINYINT UNSIGNED NOT NULL DEFAULT 0,
+
+	PRIMARY KEY (id),
+	INDEX (deleted)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+
+
+CREATE TABLE user_project_role (
+	id INT UNSIGNED AUTO_INCREMENT,
+	user_id INT UNSIGNED NOT NULL,
+	project_id INT UNSIGNED NOT NULL,
+	role_id INT UNSIGNED NOT NULL,
+	deleted TINYINT UNSIGNED NOT NULL DEFAULT 0,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES user(id),
+	FOREIGN KEY (project_id) REFERENCES project(id),
+	FOREIGN KEY (role_id) REFERENCES role(id),
+	INDEX (deleted)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+
+
+CREATE TABLE status (
+	id INT UNSIGNED AUTO_INCREMENT,
+	name VARCHAR(255) NOT NULL,
+
+	PRIMARY KEY (id)
+) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
+INSERT INTO status (id, name) VALUES (1, 'new');
+INSERT INTO status (id, name) VALUES (2, 'in_progress');
+INSERT INTO status (id, name) VALUES (3, 'testing');
+INSERT INTO status (id, name) VALUES (4, 'feedback');
+INSERT INTO status (id, name) VALUES (5, 'resolved');
+INSERT INTO status (id, name) VALUES (6, 'rejected');
+
+
+CREATE TABLE task (
+	id INT UNSIGNED AUTO_INCREMENT,
+	project_id INT UNSIGNED NOT NULL,
+	parent_task_id INT UNSIGNED,
+	author_id INT UNSIGNED NOT NULL,
+	assignee_id INT UNSIGNED,
+	status_id INT UNSIGNED NOT NULL,
+	short_description VARCHAR(255) NOT NULL,
+	long_description TEXT,
+	priority INT UNSIGNED NOT NULL DEFAULT 0,
+	deleted TINYINT UNSIGNED NOT NULL DEFAULT 0,
+
+	PRIMARY KEY (id),
+	FOREIGN KEY (project_id) REFERENCES project(id),
+	FOREIGN KEY (parent_task_id) REFERENCES task(id),
+	FOREIGN KEY (author_id) REFERENCES user(id),
+	FOREIGN KEY (assignee_id) REFERENCES user(id),
+	FOREIGN KEY (status_id) REFERENCES status(id),
 	INDEX (deleted)
 ) Engine InnoDB DEFAULT COLLATE utf8_swedish_ci;
