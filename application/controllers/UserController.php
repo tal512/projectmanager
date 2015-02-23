d1a4f17856ed1e90a010f342b3c6ea3f46c7c329
tal <tal@hunam.net>
2015-02-23 18:35:01 +0200

    Set user role when registering new user
    Default new users' roles to "member".

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -48,39 +48,79 @@ class UserController extends Controller
 
 	public function actionRegister()
 	{
+		$db = $this->container['db'];
+		$db->beginTransaction();
+
 		if (isset($_POST['email']) && isset($_POST['password']) && isset($_POST['name'])) {
 			$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 			$password = $_POST['password'];
 			$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-			$db = $this->container['db'];
-			$sql = "SELECT id FROM user WHERE email = :email AND deleted = 0";
-			$values = [':email' => $email];
-			$db->prepare($sql, $values);
-			$user = $db->query();
+			$user = $this->getUserByEmail($email);
 
-			if ($user === false) {
-				$sql = "INSERT INTO user (email, password, name) VALUES (:email, :password, :name)";
-				$values = [
-					':email' => $email,
-					':password' => password_hash($password, PASSWORD_DEFAULT),
-					':name' => $name,
-				];
-				$db->prepare($sql, $values);
-
-				if ($db->execute()) {
+			if ($user === false && $this->registerUser($email, $password, $name)) {
+				$userId = $db->lastInsertId();
+				if ($this->registerRole($userId, 'member')) {
 					echo json_encode([
 						'status' => 'success',
 						'message' => 'User registered',
 					]);
+					$db->endTransaction();
+					die;
 				}
-				die;
 			}
 		}
 
+		$db->cancelTransaction();
 		echo json_encode([
 			'status' => 'error',
 			'message' => 'Register failed',
 		]);
+	}
+
+	private function getUserByEmail($email, $deleted = 0)
+	{
+		$db = $this->container['db'];
+		$sql = "SELECT id FROM user WHERE email = :email AND deleted = :deleted";
+		$values = [':email' => $email, ':deleted' => $deleted];
+		$db->prepare($sql, $values);
+		return $db->query();
+	}
+
+	private function registerUser($email, $password, $name)
+	{
+		$db = $this->container['db'];
+
+		$sql = "INSERT INTO user (email, password, name) VALUES (:email, :password, :name)";
+		$values = [
+			':email' => $email,
+			':password' => password_hash($password, PASSWORD_DEFAULT),
+			':name' => $name,
+		];
+		$db->prepare($sql, $values);
+
+		if ($db->execute()) {
+			return true;
+		}
+		return false;
+	}
+
+	private function registerRole($userId, $role)
+	{
+		$db = $this->container['db'];
+
+		if (is_int($role)) {
+			$sql = "INSERT INTO user_role (user_id, role_id) VALUES (:user_id, :role_id)";
+			$values = [':user_id' => $userId, ':role_id' => $role];
+		} else {
+			$sql = "INSERT INTO user_role (user_id, role_id)"
+				. " SELECT :user_id, id"
+				. " FROM role"
+				. " WHERE role.name = :role";
+			$values = [':user_id' => $userId, ':role' => $role];
+		}
+
+		$db->prepare($sql, $values);
+		return $db->execute();
 	}
 }
