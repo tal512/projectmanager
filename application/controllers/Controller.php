33a3b0bd9216d8b0d5d6df830598fa80871de439
tal <tal@hunam.net>
2015-03-04 21:22:49 +0200

    Load Validator in Controller and Model

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -2,6 +2,11 @@
 
 abstract class Controller extends BaseClass
 {
+	protected function loadDependencies()
+	{
+		require_once $this->config['app']['dir'] . '/models/Validator.php';
+	}
+
 	protected function renderJson($data)
 	{
 		echo json_encode($data);
--- application/models/Model.php
+++ application/models/Model.php
@@ -2,6 +2,10 @@
 
 abstract class Model extends BaseClass
 {
+	protected function loadDependencies() {
+		require_once $this->config['app']['dir'] . '/models/Validator.php';
+	}
+
 	abstract public function save();
 	abstract public function validate();
 }
