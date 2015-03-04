c679f9c7ce61f306c3f52394111830fa875187fe
tal <tal@hunam.net>
2015-03-04 21:22:23 +0200

    Add Validator with static validation methods

---
+++ application/models/Validator.php
@@ -0,0 +1,34 @@
+<?php
+
+class Validator
+{
+	public static function booleanInteger($value) {
+		return filter_var($value, FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^[0-1]{1}$/']]);
+	}
+
+	public static function email($value)
+	{
+		return filter_var($value, FILTER_VALIDATE_EMAIL);
+	}
+
+	public static function hexadecimal($value, $allowEmpty = true)
+	{
+		return filter_var($value, FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^[a-f0-9]*$/']]);
+	}
+
+	public static function integer($value)
+	{
+		return filter_var($value, FILTER_VALIDATE_REGEXP, ['options' => ['regexp' => '/^[0-9]+$/']]);
+	}
+
+	public static function password($value)
+	{
+		$password = password_get_info($value);
+		return ($password['algo'] !== 0) ? true : false;
+	}
+
+	public static function string($value)
+	{
+		return filter_var($value, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+	}
+}
