d96d9566ced806476223ce0ce4b0f4b4100edf04
JTS-FIN <jarkko@lauspalo.com>
2015-03-06 14:32:04 +0200

    Fixed path, commenting
    Path for test api was wrong, fixed. Also added some informative
    comments, perhaps useful for angular beginners, to be perhaps
    deleted later.

--- public/app/project/services/projectServices.js
+++ public/app/project/services/projectServices.js
@@ -1,14 +1,12 @@
-/**
- * Currently not in use, due to some mysterious error
- * @todo Make this work, tutorial: 
- *   https://docs.angularjs.org/tutorial/step_11
- **/
-
+// Create a new module, projectServices, which uses ngResource
 var projectServices = angular.module('projectServices', ['ngResource']);
 
+/**
+ * Service that fetches all projects from API
+ **/
 projectServices.factory('Projects', ['$resource',
 	function ($resource) {
-		return $resource('../../../api/testdata/projects.json', {}, {
-			query: {method:'GET', isArray:true}
+		return $resource('api/testdata/projects.json', {}, {
+			query: {method: 'GET', isArray: true}
 		});
 }]);
