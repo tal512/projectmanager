8d76c4ad312a8b56d7f005c10fcf0f1c6b4e6c5c
tal <tal@hunam.net>
2015-03-08 16:33:57 +0200

    Add getter and setter to Model

--- application/models/Model.php
+++ application/models/Model.php
@@ -2,10 +2,22 @@
 
 abstract class Model extends BaseClass
 {
+	protected $attributes;
+
+	abstract public function save();
+	abstract public function validate();
+
 	protected function loadDependencies() {
 		require_once $this->config['app']['dir'] . '/models/Validator.php';
 	}
 
-	abstract public function save();
-	abstract public function validate();
+	public function __get($name)
+	{
+		return $this->attributes[$name];
+	}
+
+	public function __set($name, $value)
+	{
+		$this->attributes[$name] = $value;
+	}
 }
