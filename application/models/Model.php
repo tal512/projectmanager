5a6b7f53a82f994f2af82caef249c02cc65e1a30
tal <tal@hunam.net>
2015-03-08 16:39:24 +0200

    Add a rules variable and method to Model
    The rules variable is an array with key => value pairs, where key is
    the name of a class variable and value is the validation method used
    to validate it. The validation methods are static methods in Validator.
    
    The setRules method is used to populate the rules variable on object
    construction. By doing the population the rules variable can be used
    later to set and get object variables, for example when reading post
    data or when validating object attributes.

--- application/models/Model.php
+++ application/models/Model.php
@@ -3,9 +3,16 @@
 abstract class Model extends BaseClass
 {
 	protected $attributes;
+	protected $rules;
 
 	abstract public function save();
 	abstract public function validate();
+	abstract protected function setRules();
+
+	public function __construct(&$container) {
+		parent::__construct($container);
+		$this->setRules();
+	}
 
 	protected function loadDependencies() {
 		require_once $this->config['app']['dir'] . '/models/Validator.php';
