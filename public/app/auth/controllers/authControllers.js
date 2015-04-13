211758135e2162f58bf09d6a96c3b01336996735
JTS-FIN <jarkko@lauspalo.com>
2015-04-13 21:57:53 +0300

    To use authService

--- public/app/auth/controllers/authControllers.js
+++ public/app/auth/controllers/authControllers.js
@@ -1,4 +1,4 @@
-var authControllers = angular.module('authControllers', []);
+var authControllers = angular.module('authControllers', ['authServices']);
 
 /**
  * Very basic login controller
@@ -9,23 +9,8 @@ var authControllers = angular.module('authControllers', []);
  * https://github.com/auth0/angularjs-jwt-authentication-tutorial/tree/master/frontend
  * http://brewhouse.io/blog/2014/12/09/authentication-made-simple-in-single-page-angularjs-applications.html
  */
-authControllers.controller('LoginCtrl', ['$rootScope', '$scope', '$http', function ($rootScope, $scope, $http) {
+authControllers.controller('LoginCtrl', ['$scope', 'Authentication', function ($scope, Authentication) {
 	$scope.submit = function (user) {
-		var data = {email: user.email, password: user.password};
-		$http({
-			url: 'api/user/login/',
-			method: 'POST',
-			data: $.param(data),
-			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-		}).then(function (response) {
-			if (response.data.status === 'success') {
-				$rootScope.currentUser = {};
-				$rootScope.currentUser.email = user.email;
-				$rootScope.currentUser.authKey = response.data.authKey;
-			}
-			else {
-				alert("Login failed: " + response.data.message);
-			}
-		});
+		Authentication.login(user.email, user.password);
 	}
 }]);
