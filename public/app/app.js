28e3eb33dbc33401b2a1c7a29f9a3a2b53c70f8a
JTS-FIN <jarkko@lauspalo.com>
2015-04-13 22:00:44 +0300

    Auth to use sessionstorage + projects/add

--- public/app/app.js
+++ public/app/app.js
@@ -7,15 +7,29 @@ var projectManagerApp = angular.module('projectManagerApp', ['ui.router',
 	'projectServices',
 	'projectDirectives',
 	'navigationControllers',
-	'authControllers']);
+	'authControllers',
+	'authServices']);
 
-projectManagerApp.run(['$rootScope', '$state', '$stateParams', function ($rootScope,   $state,   $stateParams) {
+projectManagerApp.run(['$rootScope', '$state', '$stateParams', '$location', 'Authentication', 
+												function ($rootScope, $state, $stateParams, $location, Authentication) {
 	// It's very handy to add references to $state and $stateParams to the $rootScope
 	// so that you can access them from any scope within your applications.For example,
 	// <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
 	// to active whenever 'contacts.list' or one of its decendents is active.
 	$rootScope.$state = $state;
 	$rootScope.$stateParams = $stateParams;
+	
+	// Initialize the authentication service
+	Authentication.init();
+
+	$rootScope.$on('$stateChangeStart',
+	function(event, toState, toParams, fromState, fromParams) {
+		var requireLogin = toState.data.requireLogin;
+		if (requireLogin && typeof Authentication.getUserInfo() === 'undefined') {
+			event.preventDefault();
+			$state.go('login');
+		}
+	});
 }]);
 
 /**
@@ -24,7 +38,7 @@ projectManagerApp.run(['$rootScope', '$state', '$stateParams', function ($rootSc
  * and post about using it with authetication:
  * http://brewhouse.io/blog/2014/12/09/authentication-made-simple-in-single-page-angularjs-applications.html
  */
-projectManagerApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider,   $urlRouterProvider) {
+projectManagerApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
 	// Use $urlRouterProvider to configure any redirects (when) and invalid urls (otherwise).
 	$urlRouterProvider
@@ -34,8 +48,16 @@ projectManagerApp.config(['$stateProvider', '$urlRouterProvider', function ($sta
 	$stateProvider
 	.state('projects', {
 		url: '/projects',
-		templateUrl: 'app/project/views/projectListLarge.html',
-		controller: 'ProjectListCtrl',
+		templateUrl: 'app/project/views/projects.html',
+		controller: 'ProjectsCtrl',
+		data: {
+			requireLogin: true
+		}
+	})
+	.state('projectsAdd', {
+		url: '/projects/add',
+		templateUrl: 'app/project/views/projectsAdd.html',
+		controller: 'ProjectsAddCtrl',
 		data: {
 			requireLogin: true
 		}
