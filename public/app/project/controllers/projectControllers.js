7f3e6bded6a05467fa82d699224f0197add3c7c0
JTS-FIN <jarkko@lauspalo.com>
2015-04-13 21:59:37 +0300

    ProjectAddCtrl
    Also renamed ProjectListCtrl -> ProjectsCtrl

--- public/app/project/controllers/projectControllers.js
+++ public/app/project/controllers/projectControllers.js
@@ -2,8 +2,32 @@
 var projectControllers = angular.module('projectControllers', ['projectServices']);
 
 /**
- * Controller for listing all Projects
+ * Controller for listing all projects
  **/
-projectControllers.controller('ProjectListCtrl', ['$scope', 'Projects', function($scope, Projects) {
+projectControllers.controller('ProjectsCtrl', ['$scope', 'Projects', function($scope, Projects) {
 	$scope.projects = Projects.query();
+}]);
+
+/**
+ * Controller for adding a new project
+ **/
+projectControllers.controller('ProjectsAddCtrl', ['$scope', function($scope) {
+	$scope.submit = function (project) {
+		var data = {status: project.status, name: project.name, description: project.description};
+		$http({
+			url: 'api/project/create/',
+			method: 'POST',
+			data: $.param(data),
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).then(function (response) {
+			if (response.data.status === 'success') {
+				alert("Project created");
+			}
+			else {
+				alert("Login failed: " + response.data.message);
+			}
+		}, function(error) {
+      alert("Error with creating a new project");
+    });
+	}
 }]);
