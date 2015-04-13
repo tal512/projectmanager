b82667cea44ab11a828f96ec26ec46012e7f3638
JTS-FIN <jarkko@lauspalo.com>
2015-04-13 22:01:35 +0300

    Disable cache (if that even works)

--- public/app/project/services/projectServices.js
+++ public/app/project/services/projectServices.js
@@ -1,12 +1,13 @@
 // Create a new module, projectServices, which uses ngResource
-var projectServices = angular.module('projectServices', ['ngResource']);
+var projectServices = angular.module('projectServices', ['ngResource', 'authServices']);
 
 /**
  * Service that fetches all projects from API
  **/
-projectServices.factory('Projects', ['$resource',
-	function ($resource) {
+projectServices.factory('Projects', ['$resource', 'Authentication',
+	function ($resource, Authentication) {
 		return $resource('api/testdata/projects.json', {}, {
-			query: {method: 'GET', isArray: true}
+			query: {method: 'GET', isArray: true},
+			cache: false
 		});
 }]);
