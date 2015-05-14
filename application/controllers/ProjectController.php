bb7ab5b0eb54bf759f9aa9ee1035e857c8e4e49e
tal <tal@hunam.net>
2015-05-14 18:02:24 +0300

    Add getAll method to ProjectController

--- application/controllers/ProjectController.php
+++ application/controllers/ProjectController.php
@@ -34,4 +34,29 @@ class ProjectController extends Controller
 			'message' => 'Create project failed',
 		]);
 	}
+	
+	public function getAll()
+	{
+		$sql = "SELECT p.id, p.project_status_id, p.name, p.description, p.deleted, ps.name AS project_status"
+			. " FROM " . $this->getTable('Project') . " AS p"
+			. " LEFT JOIN " . $this->getTable('Project') . "_status AS ps ON ps.id = p.project_status_id"
+			. " WHERE p.deleted = 0";
+		$this->db->prepare($sql);
+		$projects = $this->db->queryAll();
+
+		if ($projects !== false) {
+			$list = [];
+			foreach($projects as $project) {
+				$list[$project->id] = new Project($this->container);
+				$list[$project->id]->id = $project['id'];
+				$list[$project->id]->projectStatusId = $project['project_status_id'];
+				$list[$project->id]->name = $project['name'];
+				$list[$project->id]->description = $project['description'];
+				$list[$project->id]->deleted = $project['deleted'];
+				$list[$project->id]->projectStatus = $project['project_status'];
+			}
+			return $list;
+		}
+		return false;
+	}
 }
